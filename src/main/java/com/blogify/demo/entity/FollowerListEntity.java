7def4a695f877b147e4ee1b302e76ca7f93a3aa5
rawatsaheb5 <diamondcut2004@gmail.com>
2025-12-13 12:19:37 +0530

    created followerlist entity

---
+++ src/main/java/com/blogify/demo/entity/FollowerListEntity.java
@@ -0,0 +1,39 @@
+package com.blogify.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "follower_list", 
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"followee_id", "follower_id"})
+        }
+)
+@Data
+@NoArgsConstructor
+public class FollowerListEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "followee_id" , nullable = false)
+    private UserEntity followee;
+    @ManyToOne
+    @JoinColumn(name = "follower_id", nullable = false)
+    private UserEntity follower;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
